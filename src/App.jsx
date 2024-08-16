bdf987faf6958e2d000b91cdd8af6a4deadf0168
PatrickFS18 <patrick.fs.dev@gmail.com>
2024-08-15 21:11:46 -0300

    atualizando

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./style/style.css";
 
 import About from "./pages/about";
 import Index from "./pages";
+import Siapreps from "./pages/siapreps";
+import Phytobloom from "./pages/phytobloom";
 
 function App() {
   return (
     <BrowserRouter>
-       <Routes>
-        <Route path="/" element={<Index/>}/>
-        <Route path="/about" element={<About/>}/>
-       </Routes>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/siapreps" element={<Siapreps />} />
+        <Route path="/phytobloom" element={<Phytobloom />} />
+
+      </Routes>
     </BrowserRouter>
   )
 }
