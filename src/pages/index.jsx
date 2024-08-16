41ad6aa69660545a595193b81475f44d09298383
PatrickFS18 <patrick.fs.dev@gmail.com>
2024-08-15 21:14:58 -0300

    ajustes na tela principal

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,45 +1,43 @@
-import React, {useRef,useLayoutEffect} from "react";
+import React, { useRef, useLayoutEffect } from "react";
 import Header from "../components/layouts/header";
 import Card from "../components/common/card";
-
 import mar from "../imgs/mar.png";
 import wave from "../imgs/wave.svg";
 import siaprepsMonitor from "../imgs/siaprepsMonitor.svg";
 import satelite from "../imgs/satelite.svg";
 import lupa from "../imgs/lupa.svg";
 import mapa from "../imgs/mapa.svg";
-
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 
-export default function Index(){
+export default function Index() {
 
     const el = useRef(null);
 
-    useLayoutEffect(() =>{
+    useLayoutEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
 
-        gsap.context(() =>{
+        gsap.context(() => {
             gsap.timeline({
-                scrollTrigger:{
-                   scrub:true,
-                   start: "top 20px",
-                   end: "bottom 1000px"
+                scrollTrigger: {
+                    scrub: true,
+                    start: "top 20px",
+                    end: "bottom 1000px"
                 }
             })
-            .fromTo("#card-1", { x: 120, opacity:0}, { x:0, opacity:1 })
-            .fromTo("#card-2", { x: 120, opacity:0}, { x:0, opacity:1 })
-            .fromTo("#card-3", { x: 120, opacity:0}, { x:0, opacity:1 })
-        },el.current);
+                .fromTo("#card-1", { x: 120, opacity: 0 }, { x: 0, opacity: 1 })
+                .fromTo("#card-2", { x: 120, opacity: 0 }, { x: 0, opacity: 1 })
+                .fromTo("#card-3", { x: 120, opacity: 0 }, { x: 0, opacity: 1 })
+        }, el.current);
 
         return () => gsap.killTweensOf(".helps");
     })
 
-    return(
+    return (
         <>
             <section className="header_container">
-                <img src={mar} alt="Imagem de fundo" className="bg_img"/>
-                <Header/>
+                <img src={mar} alt="Imagem de fundo" className="bg_img" />
+                <Header />
                 <article className="article_header_container">
                     <img src={siaprepsMonitor} alt="Imagem de um monitor da siapreps" />
                     <h2>
@@ -51,7 +49,7 @@ export default function Index(){
                     </p>
                 </article>
                 <footer className="footer_header_container">
-                    <img src={wave} alt="Ondas do rodapé"/>
+                    <img src={wave} alt="Ondas do rodapé" />
                     <article>
                         <div>
                             <h2 className="title">
