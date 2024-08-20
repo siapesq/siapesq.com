685cd250e06378f546d492c441d2b2be791fa823
PatrickFS18 <patrick.fs.dev@gmail.com>
2024-08-20 12:46:48 -0300

    alguns acrescimos

--- src/components/sections/siapreps.jsx
+++ src/components/sections/siapreps.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useLayoutEffect } from "react";
 import siapreps from "../../imgs/siaprepsLogo.png";
 import iconChecked from "../../imgs/iconChecked.png";
+import tubarao from "../../imgs/tubarao.png";
 export default function SectionSiapreps() {
 
 
@@ -20,6 +21,16 @@ export default function SectionSiapreps() {
                             <li><img className="iconChecked" src={iconChecked} width={60} /> Mapa de bordo automatizado</li>
                         </ul>
                     </div>
+                    <div className="tubarao">
+                        <img src={tubarao} alt="Tubarão" />
+                    </div>
+                </div>
+                <div className="siapreps_bottom">
+                    <div className="siapreps_texto">
+                        <h2>A plataforma SIAPREPS disponibiliza aos usuários mapas que  permitem</h2>
+                        <h2 style={{ marginLeft: '50px' }}>localizar  e monitorar os cardumes com maior facilidade e precisão</h2>
+                    </div>
+                    <button className="btn_more">Saiba mais</button>
                 </div>
             </section>
 
