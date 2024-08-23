7cf588afc080df3e9da2fb5ee02523dfeed93a3a
gabriel <gabrieljoanol100@hotmail.com>
2024-08-22 23:18:48 -0300

    header como componente

--- src/pages/siapreps.jsx
+++ src/pages/siapreps.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import Header from "../components/layouts/header";
-import cardume from "../imgs/cardume.png";
-// import cardume2 from "../imgs/cardume2.png";
-// import peixes from "../imgs/peixes.png";
-import siapresqPc from "../imgs/siapresqPc.svg";
-// import siapreps from "../imgs/siapreps.svg";
-// import logo from "../imgs/logo.svg";
+// import Header from "../components/layouts/header";
+// import cardume from "../imgs/cardume.png";
+// import siapresqPc from "../imgs/siapresqPc.svg";
 import SectionBarco from "../components/sections/siaprepsBarco";
 import SectionPlataforma from "../components/sections/siaprepsPlataforma";
 import SectionCardume from "../components/sections/siaprepsCardume";
 import SectionformS from "../components/sections/siaprepsFormS";
 import SectionFooter from "../components/sections/siaprepsFooter";
-import '../style/siapreps/siapreps.css';
+import SectionHeader from "../components/sections/siaprepsHeader";
 
 
 export default function Index(){
@@ -19,23 +15,14 @@ export default function Index(){
     
     return(
         <>
-            <section className="header_container">
-                <img src={cardume} alt="Imagem de fundo" className="cardume"/>
-                <Header/>
-                <article className="article_header_container">
-                <h1 id="siapreps" >
-                    SIAPREPS
-                </h1>
-                    <p id="textosiapreps">
-                    A plataforma SIAPREPS disponibiliza aos usuários mapas que  permitem localizar  e monitorar os cardumes com maior facilidade e precisão
-                     </p>
-                    <img id="notesiapreps" src={siapresqPc} alt="Imagem de um notebook da siapreps" />
-                     <button className="fundoInvinsivel"> Começar Agora</button> 
-                </article>
-            </section>
+                <div>
+                <SectionHeader />
+                </div>
+
                 <div >
                 <SectionBarco />
                 </div>
+           
 
                 <div >
                 <SectionPlataforma />
