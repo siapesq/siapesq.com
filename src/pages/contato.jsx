fa94f7a12b2db36a8db0ed73d6858d044a76dab7
gabriel <gabrieljoanol100@hotmail.com>
2024-08-26 23:29:27 -0300

    contato pronto falta arrumar header para branco

---
+++ src/pages/contato.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SectionContato from "../components/sections/formContato";
+
+
+export default function Contato() {
+    return (
+        <>
+       
+       <div>
+         <SectionContato />
+       </div>
+       
+    
+        </>
+    );
+}
