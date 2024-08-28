b79f6dd4db2dbed6d80bcaa1552b1b97350ed54c
gabriel <gabrieljoanol100@hotmail.com>
2024-08-27 21:24:06 -0300

    peixes

--- src/components/sections/siaprepsCardume.jsx
+++ src/components/sections/siaprepsCardume.jsx
@@ -40,7 +40,18 @@
 
 
 import React, { useState } from 'react';
-import peixe from "../../imgs/peixe.svg";
+import meka from "../../imgs/Meka.png";
+import atumoludo from "../../imgs/Atum Olhudo BAT.png";
+import atumalbacora from "../../imgs/Atum Albacora.png";
+import atumyellowfin from "../../imgs/Atum YellowFin.png";
+import bonito from "../../imgs/Bonito Listrado.png";
+import pescada from "../../imgs/Pescada.png";
+import anchova from "../../imgs/Anchova .png";
+import corvina from "../../imgs/Corvina .png";
+import abrotea from "../../imgs/Abrótea.png";
+import merluza from "../../imgs/Merluza.png";
+import tainha from "../../imgs/Tainha.png";
+import sardinha from "../../imgs/Sardinha.png";
 import '../../style/siapreps/siapreps_cardume/Carrossel.css';
 
 const Carrossel = () => {
@@ -48,18 +59,18 @@ const Carrossel = () => {
 
   const equipe = [
     // { nome: 'Meka', nomeCientifico: 'Xiphias gladius', imagem: {peixe} }, 
-    { nome: 'Meka', nomeCientifico: 'Xiphias gladius', imagem:peixe },
-    { nome: 'Atum Olhudo BAT', nomeCientifico: 'Thunnus obesus', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Atum Albacora', nomeCientifico: 'Thunnus alalunga', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Atum YellowFin', nomeCientifico: 'Thunnus alalunga', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Bonito Listrado', nomeCientifico: 'Katsuwonus pelamis', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Pescada', nomeCientifico: 'Merluccius merluccius', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Anchova', nomeCientifico: 'Pomatomus Saltatrix', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Corvina ', nomeCientifico: 'Micropogonias funieri', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Abrótea', nomeCientifico: 'Urophycis brasiliensis', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Merluza', nomeCientifico: 'merluccius hubbsi', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Tainha', nomeCientifico: 'Mugil liza', imagem: 'https://picsum.photos/200/300' },
-    { nome: 'Sardinha', nomeCientifico: 'Sardinella brasiliensis', imagem: 'https://picsum.photos/200/300' },
+    { nome: 'Meka', nomeCientifico: 'Xiphias gladius', imagem:meka },
+    { nome: 'Atum Olhudo BAT', nomeCientifico: 'Thunnus obesus', imagem: atumoludo },
+    { nome: 'Atum Albacora', nomeCientifico: 'Thunnus alalunga', imagem: atumalbacora },
+    { nome: 'Atum YellowFin', nomeCientifico: 'Thunnus alalunga', imagem: atumyellowfin },
+    { nome: 'Bonito Listrado', nomeCientifico: 'Katsuwonus pelamis', imagem: bonito },
+    { nome: 'Pescada', nomeCientifico: 'Merluccius merluccius', imagem: pescada },
+    { nome: 'Anchova', nomeCientifico: 'Pomatomus Saltatrix', imagem: anchova },
+    { nome: 'Corvina ', nomeCientifico: 'Micropogonias funieri', imagem: corvina },
+    { nome: 'Abrótea', nomeCientifico: 'Urophycis brasiliensis', imagem: abrotea },
+    { nome: 'Merluza', nomeCientifico: 'merluccius hubbsi', imagem: merluza },
+    { nome: 'Tainha', nomeCientifico: 'Mugil liza', imagem: tainha },
+    { nome: 'Sardinha', nomeCientifico: 'Sardinella brasiliensis', imagem: sardinha },
   ];
 
   const nextSlide = () => {
@@ -96,6 +107,7 @@ const Carrossel = () => {
       <div>
     </div>
       <h2 className="cardumeh2">Obs: Nós filtramos áreas com menor probabilidade de haver animais proibidos de pesca, como tubarões, baleias, e tartarugas</h2>
+      <h2 className='cardumeh2'>Nossas indicações levam em consideração a restrição de áreas de capturas onde habitam:</h2>
       </div>
   );
 };
