84ab37feda802b42c5895e69962fd3e942ee0df6
Victor-C-B-Santana <player.17112008@gmail.com>
2024-09-04 19:11:24 -0300

    Adicionando detalhes ao index
    diminuição do menu e footer, aumento dos titulos e fontes e a adição dos 3 pontos em cima do titulo da section 2 no index

--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Card({id,photo,title,content}){
     return(
         <section id={id} className="card">
-            <img src={photo} alt="foto descritiva"/>
+            <div>
+                <img src={photo} alt="foto descritiva"/>
+            </div>
             <h2 className="title">
                 {title}
             </h2>
