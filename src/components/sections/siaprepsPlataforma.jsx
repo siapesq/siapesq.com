4709657c730aeed5427e092191e0c0d0fc69572a
Victor-C-B-Santana <85232183+Victor-C-B-Santana@users.noreply.github.com>
2024-10-06 09:04:22 -0300

    adicionando links nos botões começar agora
    Apenas adicionando o <a> (sem estilo)

--- src/components/sections/siaprepsPlataforma.jsx
+++ src/components/sections/siaprepsPlataforma.jsx
@@ -14,7 +14,7 @@ export default function SectionPlataforma(){
                 <img id="plataforma" src={siapreps} alt="Imagem de platforma siapreps" />
                 {/* <h1 id="azulzinha">A plataforma SIAPREPS disponibiliza aos usuários mapas que  permitem localizar  e monitorar os cardumes com maior facilidade e precisão.</h1> */}
                 <img src={texto} alt="" id="textoazul" />
-                <button className="fundoInvinsivel2"> Começar Agora</button> 
+                <button className="fundoInvinsivel2"> <a href="https://wa.me/555399503671" target="_blank" rel="noopener noreferrer">Começar Agora</a></button> 
                 </article>
                 </section>
         </>
