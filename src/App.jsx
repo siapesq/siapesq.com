9736b88da2fb50082a7441ea6f7d17e860d147c0
PatrickFS18 <patrick.fs.dev@gmail.com>
2024-10-07 15:58:19 -0300

    fix errors in prod (url) with hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,25 @@
-// import { React, useEffect, useState } from "react";
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./style/style.css";
 import "./style/siapreps/siapreps_section/style.css";
-// import About from "./pages/about";
+import About from "./pages/about";
 import Index from "./pages";
 import Siapreps from "./pages/siapreps";
 import Phytobloom from "./pages/phytobloom";
 import Contato from "./pages/contato";
 
 function App() {
-
-  // const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  // useEffect(() => {
-
-  //   const handleResize = () => {
-  //     setWindowWidth(window.innerWidth);
-  //   }
-
-  //   window.addEventListener('resize', handleResize);
-
-  //   return () => {
-  //     window.removeEventListener('resize', handleResize);
-  //   };
-
-  // }, [windowWidth]);
-
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Index />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/about" element={<About />} />
         <Route path="/siapreps" element={<Siapreps />} />
         <Route path="/phytobloom" element={<Phytobloom />} />
         <Route path="/contato" element={<Contato />} />
-
       </Routes>
-    </BrowserRouter>
-  )
+    </Router>
+  );
 }
 
 export default App;
