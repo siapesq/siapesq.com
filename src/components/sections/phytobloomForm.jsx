7c90149885cd29aa351e56e9547b8dbf4338b705
Victor-C-B-Santana <85232183+Victor-C-B-Santana@users.noreply.github.com>
2024-10-21 21:45:55 -0300

    Fix - Ajustando a phytobloom
    Fazendo diminuindo o espaçamento do header da phytobloom.
    
    Ajustando a indentação.

--- src/components/sections/phytobloomForm.jsx
+++ src/components/sections/phytobloomForm.jsx
@@ -2,39 +2,39 @@ import React from "react";
 import '../../style/phytobloom/form.css';
 
 
-export default function SectionForm(){
+export default function SectionForm() {
 
-    
-    return(
-        <>
-     <div className="contact-container phytobloomContact">
+
+  return (
+    <>
+      <div className="contact-container phytobloomContact">
         <div id="textoform">
-        <h1 id="h1Phytobloom">Entre em contato conosco!</h1>
+          <h1 id="h1Phytobloom">Entre em contato conosco!</h1>
         </div>
-      <div className="form-content">
-        <form id="formPhytobloom">
-          <div className="input-group">
-            <div className="input-item">
-              <label htmlFor="nome">Nome</label>
-              <input type="text" id="nome" placeholder="Nome" />
+        <div className="form-content">
+          <form id="formPhytobloom">
+            <div className="input-group">
+              <div className="input-item">
+                <label htmlFor="nome">Nome</label>
+                <input type="text" id="nome" placeholder="Nome" />
+              </div>
+              <div className="input-item">
+                <label htmlFor="sobrenome">Sobrenome</label>
+                <input type="text" id="sobrenome" placeholder="Sobrenome" />
+              </div>
             </div>
             <div className="input-item">
-              <label htmlFor="sobrenome">Sobrenome</label>
-              <input type="text" id="sobrenome" placeholder="Sobrenome" />
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" placeholder="Email" />
             </div>
-          </div>
-          <div className="input-item">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Email" />
-          </div>
-          <div className="input-item">
-            <label htmlFor="mensagem">Mensagem</label>
-            <textarea id="mensagem" placeholder="Mensagem"></textarea>
-          </div>
-          <button id="botaoPhytobloom"> enviar</button>
-        </form>
+            <div className="input-item">
+              <label htmlFor="mensagem">Mensagem</label>
+              <textarea id="mensagem" placeholder="Mensagem"></textarea>
+            </div>
+            <button id="botaoPhytobloom"> enviar</button>
+          </form>
+        </div>
       </div>
-    </div>
-        </>
-      )
-    }
+    </>
+  )
+}
