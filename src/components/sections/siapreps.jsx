e5c9830fe8d44d474783703cde9e21257bd8ecac
Victor-C-B-Santana <85232183+Victor-C-B-Santana@users.noreply.github.com>
2024-10-21 21:43:29 -0300

    Fix - Ajustando o header e Header do siapreps
    Removendo a checagem de tamanho da tela do js para o css, para que não seja necessário recarregar a página.
    
    Ajustando o header da index para ajustar os textos por meio do css

--- src/components/sections/siapreps.jsx
+++ src/components/sections/siapreps.jsx
@@ -5,6 +5,7 @@ import tubarao from "../../imgs/tubarao.png";
 import peixes from "../../imgs/peixes.svg";
 import tubarao2 from "../../imgs/tubarao2.png";
 import { NavLink } from "react-router-dom";
+
 export default function SectionSiapreps() {
 
 
@@ -12,13 +13,7 @@ export default function SectionSiapreps() {
         <section className="siapreps">
             <div className="siapreps_container">
                 <div className="siapreps_logo">
-                    {
-                        (window.innerWidth > 1366) ? (
-                            <img src={siapreps} alt="Logo da Siapreps" width="565px" height="294" />)
-                            : (
-                                <div><img src={siapreps} alt="Logo da Siapreps" width="565px" height="294" /></div>
-                            )
-                    }
+                    <div><img src={siapreps} alt="Logo da Siapreps" width="565px" height="294" /></div>
                 </div>
 
                 <div className="list-column">
@@ -34,32 +29,14 @@ export default function SectionSiapreps() {
             </div>
             <div className="siapreps_bottom">
                 <div className="siapreps_texto">
-                    { (window.innerWidth > 1024) ? (
-                        <>
-                        <h2>A plataforma SIAPREPS disponibiliza aos usuários mapas que  permitem</h2>
-                        <h2 style={{ marginLeft: '50px' }}>localizar  e monitorar os cardumes com maior facilidade e precisão</h2>
-                        </>
-                    ) : (
-                        <p>A plataforma SIAPREPS disponibiliza aos usuários mapas que  permitem localizar  e monitorar os cardumes com maior facilidade e precisão</p>
-                    )
-                    }
+
+                    <p>A plataforma SIAPREPS disponibiliza aos usuários mapas que  permitem localizar  e monitorar os cardumes com maior facilidade e precisão</p>
                 </div>
                 <div className="btn_more">
                     <div id="siapreps-enviar">
-                        { (window.innerWidth > 1366 ? (
-                            <>
-                                <div className="tubaraoB"><img width="300px" src={tubarao2} alt=""/></div>
-                                <div><NavLink to={"/siapreps"} id="button">Saiba mais</NavLink></div>
-                                <div className="peixes"><img width="300px" src={peixes} alt=""/></div>
-                            </>
-                        ): (
-                            <>
-                                <img width="300px" src={tubarao2} alt="" className="tubaraoB" />
-                                <NavLink to={"#cardume"} id="button" className="saiba-mais">Saiba mais</NavLink>
-                                <img width="300px" src={peixes} alt="" className="peixes" />
-                            </>
-                        )
-                        )}
+                        <div className="tubaraoB"><img src={tubarao2} alt="" /></div>
+                        <div><NavLink to={"/siapreps"} id="button">Saiba mais</NavLink></div>
+                        <div className="peixes"><img src={peixes} alt="" /></div>
                     </div>
                 </div>
             </div>
