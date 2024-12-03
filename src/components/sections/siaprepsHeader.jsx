9a93cf38462663aeb28a7ccf467e7385e8b7883c
Gabriel <gabrieljoanol100@hotmail.com>
2024-12-03 10:34:06 -0300

    teste animação
    vou colocar outras agora

--- src/components/sections/siaprepsHeader.jsx
+++ src/components/sections/siaprepsHeader.jsx
@@ -2,30 +2,44 @@ import React from "react";
 import Header from "../layouts/header";
 import cardume from "../../imgs/cardume.png";
 import siapresqPc from "../../imgs/siapresqPc.svg";
-import siaprepsNome from "../../imgs/NomeSiapreps.svg"
+import siaprepsNome from "../../imgs/NomeSiapreps.svg";
 import '../../style/siapreps/siapreps_Header/Header.css';
+import 'animate.css';
 
-
-export default function SectionHeader(){
-
-    
-    return(
-        <>
-         <section className="headerContainer">
-                <img src={cardume} alt="Imagem de fundo" className="cardume" id="cardume"/>
-                <Header/>
-                <article className="articleHeaderContainer">
-                <h1 id="siapreps" >
-                        <img src={siaprepsNome} alt="" id="imgsiapreps" />
+export default function SectionHeader() {
+    return (
+        <section className="headerContainer">
+            <img
+                src={cardume}
+                alt="Imagem de fundo"
+                className="cardume animate__animated animate__fadeIn animate__delay-0.0.1s"
+                id="cardume"
+            />
+            <Header />
+            <article className="articleHeaderContainer">
+                <h1 id="siapreps" className="animate__animated animate__zoomIn">
+                    <img src={siaprepsNome} alt="SIAPREPS" id="imgsiapreps" />
                 </h1>
-                    <p id="textosiapreps">
-                    A plataforma SIAPREPS disponibiliza aos usuários mapas que  permitem localizar  e monitorar os cardumes com maior facilidade e precisão
-                     </p>
-                    <img id="notesiapreps" src={siapresqPc} alt="Imagem de um notebook da siapreps" />
-                     <button className="botaoSiapreps"> <a href="https://wa.me/555399503671" target="_blank" rel="noopener noreferrer"> Começar Agora </a></button>
-                </article>
-            </section>
-   
-        </>
-      )
-    }
+                <p id="textosiapreps" className="animate__animated animate__fadeIn">
+                    A plataforma SIAPREPS disponibiliza aos usuários mapas que permitem
+                    localizar e monitorar os cardumes com maior facilidade e precisão.
+                </p>
+                <img
+                    id="notesiapreps"
+                    src={siapresqPc}
+                    alt="Imagem de um notebook da siapreps"
+                    className="animate__animated animate__slideInUp"
+                />
+                <button className="botaoSiapreps animate__animated animate__bounceIn">
+                    <a
+                        href="https://wa.me/555399503671"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Começar Agora
+                    </a>
+                </button>
+            </article>
+        </section>
+    );
+}
