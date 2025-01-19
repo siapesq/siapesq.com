451a89007e4dba99a4491c91f2285bff625fee1f
PatrickFS18 <patrick.fs.dev@gmail.com>
2025-01-19 20:01:51 -0300

    download app by "Começar Agora" button

--- src/components/sections/phytobloomHeader.jsx
+++ src/components/sections/phytobloomHeader.jsx
@@ -5,25 +5,26 @@ import Phytobloom from "../../imgs/PhytobloomNome.svg"
 import '../../style/phytobloom/header.css';
 
 
-export default function SectionHeader(){
+export default function SectionHeader() {
 
-    
-    return(
+
+    return (
         <>
-         <section className="headerContainer">
-                <img src={hidro} alt="Imagem de fundo" className="hidro"/>
-                <Header/>
+            <section className="headerContainer">
+                <img src={hidro} alt="Imagem de fundo" className="hidro" />
+                <Header />
                 <article className="article-header-container">
-                <img src={Phytobloom} alt="a" id="phytobloom"  />
-                {/* <iframe src='https://my.spline.design/untitled-0db1be3080710c809a983f6ac647274f/' frameborder='0' width='100%' height='100%'></iframe> */}
-            
+                    <img src={Phytobloom} alt="a" id="phytobloom" />
+                    {/* <iframe src='https://my.spline.design/untitled-0db1be3080710c809a983f6ac647274f/' frameborder='0' width='100%' height='100%'></iframe> */}
+
                     <p id="textophytobloom">
-                    Com a PHYTOBLOOM VISION identificamos Desregulações Ambientais em Reservatórios Hídricos, propiciando o Manejo Ambiental para Evitar Eventos Críticos!
-                     </p>
-                     <button className="fundoInvinsivel"> <a href="https://wa.me/555399503671" target="_blank" rel="noopener noreferrer">Começar Agora</a></button> 
+                        Com a PHYTOBLOOM VISION identificamos Desregulações Ambientais em Reservatórios Hídricos, propiciando o Manejo Ambiental para Evitar Eventos Críticos!
+                    </p>
+                    <button className="fundoInvinsivel"> <a href="https://github.com/siapesq/phybloom-release-mirror/releases/download/v2.0.0/phytobloom-Setup-2.0.0.exe" target="_blank" rel="noopener noreferrer">Começar Agora</a></button>
+                    <p>Disponível para: <b>Windows 10 e 11</b></p>
                 </article>
             </section>
-   
+
         </>
-      )
-    }
+    )
+}
