e1bf130bcc3c7bfec4858e91ac8059e45e8d9578
Gularte <47986817+thgularte@users.noreply.github.com>
2025-05-02 11:38:43 -0300

    att

--- src/components/sections/siaprepsHeader.jsx
+++ src/components/sections/siaprepsHeader.jsx
@@ -1,45 +1,54 @@
-import React from "react";
-import Header from "../layouts/header";
-import cardume from "../../imgs/cardume.png";
-import siapresqPc from "../../imgs/siapresqPc.svg";
-import siaprepsNome from "../../imgs/NomeSiapreps.svg";
-import '../../style/siapreps/siapreps_Header/Header.css';
 import 'animate.css';
+import React from "react";
+import cardume from "../../imgs/cardume.png";
+import siaprepsNome from "../../imgs/NomeSiapreps.svg";
+import siapresqPc from "../../imgs/siapresqPc.svg";
+import '../../style/siapreps/siapreps_Header/Header.css';
+import Header from "../layouts/header";
 
 export default function SectionHeader() {
     return (
-        <section className="headerContainer">
-            <img
-                src={cardume}
-                alt="Imagem de fundo"
-                className="cardume animate__animated animate__fadeIn animate__delay-0.0.1s"
-                id="cardume"
-            />
-            <Header />
-            <article className="articleHeaderContainer">
-                <h1 id="siapreps" className="animate__animated animate__zoomIn">
-                    <img src={siaprepsNome} alt="SIAPREPS" id="imgsiapreps" />
-                </h1>
-                <p id="textosiapreps" className="animate__animated animate__fadeIn">
-                    A plataforma SIAPREPS disponibiliza aos usuários mapas que permitem
-                    localizar e monitorar os cardumes com maior facilidade e precisão.
-                </p>
+        <>
+            <section className="headerContainer">
                 <img
-                    id="notesiapreps"
-                    src={siapresqPc}
-                    alt="Imagem de um notebook da siapreps"
-                    className="animate__animated animate__slideInUp"
+                    src={cardume}
+                    alt="Imagem de fundo"
+                    className="cardume animate__animated animate__fadeIn animate__delay-0.0.1s"
+                    id="cardume"
                 />
-                <button className="botaoSiapreps animate__animated animate__bounceIn">
-                    <a
-                        href="https://wa.me/555399503671"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Começar Agora
-                    </a>
-                </button>
-            </article>
-        </section>
+                <Header />
+                <article className="articleHeaderContainer">
+                    <h1 id="siapreps" className="animate__animated animate__zoomIn">
+                        <img src={siaprepsNome} alt="SIAPREPS" id="imgsiapreps" />
+                    </h1>
+                    <p id="textosiapreps" className="animate__animated animate__fadeIn">
+                        A plataforma SIAPREPS disponibiliza aos usuários mapas que permitem
+                        localizar e monitorar os cardumes com maior facilidade e precisão.
+                    </p>
+                    <img
+                        id="notesiapreps"
+                        src={siapresqPc}
+                        alt="Imagem de um notebook da siapreps"
+                        className="animate__animated animate__slideInUp"
+                    />
+
+                    <div className="botaoContainer">
+                        <button className="botaoSiapreps animate__animated animate__bounceIn">
+                            <a
+                                href="https://github.com/siapesq/siapreps-release-mirror/releases/download/v1.0.20/SIAPREPS-Setup-1.0.20.exe"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Baixar Software
+                            </a>
+                        </button>
+                        <p className="versaoSiapreps">
+                            Versão 1.0.20 <br /> Disponível para:   Windows 10 e 11
+                        </p>
+                    </div>
+
+                </article>
+            </section>
+        </>
     );
 }
